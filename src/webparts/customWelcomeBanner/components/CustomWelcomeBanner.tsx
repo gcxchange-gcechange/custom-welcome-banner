0b35829e9405680197fb20ffbd1f68be5db3a995
RVany <vanmathyrr@gmail.com>
2022-07-11 09:09:04 -0400

    Localization updated

--- src/webparts/customWelcomeBanner/components/CustomWelcomeBanner.tsx
+++ src/webparts/customWelcomeBanner/components/CustomWelcomeBanner.tsx
@@ -2,31 +2,56 @@ import * as React from 'react';
 import styles from './CustomWelcomeBanner.module.scss';
 import { ICustomWelcomeBannerProps } from './ICustomWelcomeBannerProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-import * as strings from 'CustomWelcomeBannerWebPartStrings';
+// import * as strings from 'CustomWelcomeBannerWebPartStrings';
+import { SelectLanguage } from  './SelectLanguage';
+
 
 export default class CustomWelcomeBanner extends React.Component<ICustomWelcomeBannerProps, {}> {
-  public render(): React.ReactElement<ICustomWelcomeBannerProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+  constructor(props: ICustomWelcomeBannerProps) {
+    super(props);
 
+    this.state = {
+      groups: [],
+      
+    };
+  }
+  
+
+  public strings = SelectLanguage(this.props.prefLang);
+  public render(): React.ReactElement<ICustomWelcomeBannerProps> {
+    const {    
+      hasTeamsContext,
+      userDisplayName,
+      prefLang
+    } = this.props;    
+    
     return (
       <section className={`${styles.customWelcomeBanner} ${hasTeamsContext ? styles.teams : ''}`}>
+        <p>testing:{prefLang}</p>
         <div className={styles.welcome}>
-          <div className={styles.title}>{strings.WelcomeTitle}{escape(userDisplayName)}</div>
+          <div className={styles.title}>{this.strings.WelcomeTitle}{escape(userDisplayName)}</div>
           <div className={styles.welcomeMessageContainer}>
-            <div className={styles.welcomeMessage}>{strings.WelcomeMessage} </div>
+            <div className={styles.welcomeMessage}>{this.strings.WelcomeMessage} </div>
             <div className={styles.headerBackgroundImagePlaceHolder}></div>
           </div>
           <div className={styles.button}>
-            <a href={strings.AboutGcxchangeURL} target="_blank" rel="noreferrer">{strings.AboutGcxchangeText}</a>
+            <a href={this.strings.AboutGcxchangeURL} target="_blank" rel="noreferrer">{this.strings.AboutGcxchangeText}</a>
           </div>
         </div>
       </section>
     );
   }
+  public componentDidMount (): void {
+    this._getGroups();
+    
+  }
+
+  public _getGroups = (): void => {
+    GroupService.getGroups().then(groups => {
+      this.setState({
+        groups: groups
+      });
+    });
+  }
+
 }
